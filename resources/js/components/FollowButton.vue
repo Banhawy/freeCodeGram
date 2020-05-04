66c20a1344c464b32f3585731272b8cea7f56038
Adham El Banhawy <the.benhawy@gmail.com>
2020-05-04 20:54:31 +0200

    created a follow button component

---
+++ resources/js/components/FollowButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button type="button" class="btn btn-primary ml-5" @click="followUser">Follow</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["userId"],
+  mounted() {
+    console.log("Component mounted.");
+  },
+
+  methods: {
+    followUser() {
+      axios.post("/follow/" + this.userId ).then(respose => alert(respose.data));
+    }
+  }
+};
+</script>

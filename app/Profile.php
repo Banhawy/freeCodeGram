e1c4942e4671c348734183b1871f51902385b83f
Adham El Banhawy <the.benhawy@gmail.com>
2020-05-04 19:46:16 +0200

    Added profile method to return profile image

--- app/Profile.php
+++ app/Profile.php
@@ -8,6 +8,14 @@ class Profile extends Model
 {
     protected $guarded = [];
 
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/84EVj0y1jIL1DoZzsHyiQwWWqFjGAF55PkrSBVqU.png';
+
+        return '/storage/' . $imagePath;
+    }
+
     public function user() {
         return $this->belongsTo(User::class);
     }

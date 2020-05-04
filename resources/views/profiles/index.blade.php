72f2cc4c47699b2ec2764f0158e9b319d3dae3d1
Adham El Banhawy <the.benhawy@gmail.com>
2020-05-04 14:06:38 +0200

    Added loop to show stored posts on profile

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -10,10 +10,11 @@
             <div>
                 <div class="d-flex justify-content-between align-items-baseline">
                     <h1>{{ $user->username }}</h1>
-                    <a href="#">Add New Post</a>
+                    <a href="/p/create">Add New Post</a>
                 </div>
+                <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
                 <div class="d-flex">
-                    <div class="pr-5"><strong>293</strong> posts</div>
+                    <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                     <div class="pr-5"><strong>46.2K</strong> followers</div>
                     <div class="pr-5"><strong>247</strong> following</div>
                 </div>
@@ -23,17 +24,15 @@
             <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
     </div>
+    <div class="row">
+        @foreach ( $user->posts as $post)
+            <div class="col-4 pt-5 pb-4">
+                <a href="/p/{{ $post->id }}">
+                    <img src="/storage/{{ $post->image }}" class="w-100" >
+                </a>
+            </div>
+        @endforeach
 
-    <div class="row pt-5">
-        <div class="col-4">
-            <img src="https://scontent-ort2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.55.847.847a/s640x640/95032566_2590953614493815_1061141814829510346_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_cat=111&_nc_ohc=8ikzr5PVEN4AX-TT5sp&oh=488702f0f3e38598197aa884645a6e3c&oe=5ED7D34F" class="w-100" alt="post1">
-        </div>
-        <div class="col-4">
-            <img src="https://scontent-ort2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c2.0.745.745a/s640x640/95224410_237873460790480_2374979867183673557_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_cat=108&_nc_ohc=X3GOBycMXucAX9IQf8K&oh=fce1c35d20906506e9e19e07bbd2e1a5&oe=5ED79328" class="w-100" alt="post2">
-        </div>
-        <div class="col-4">
-            <img src="https://scontent-ort2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/94892216_228412868421097_4113969397730546378_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_cat=101&_nc_ohc=NaBk0OlvCYQAX8dQOjk&oh=7a0287d73d969bc68fe962e7e76013e9&oe=5ED6C4CE" class="w-100" alt="post3">
-        </div>
     </div>
 </div>
 @endsection

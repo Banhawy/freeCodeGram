f9c7d5e8c4e9ecf02b66fd3303ff3d06e7d65830
Adham El Banhawy <the.benhawy@gmail.com>
2020-05-04 14:52:32 +0200

    Added edit profile functionality

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\User; 
+use App\User;
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
@@ -14,10 +14,33 @@ class ProfilesController extends Controller
      */
     public function index($user)
     {
+        // Inefficent way to find user
         $user = User::findOrFail($user);
 
         return view('profiles.index', [
             'user' => $user
         ]);
+
     }
+
+    // Better way to find user & return view
+    public function edit(\App\User $user)
+    {
+        return view("profiles.edit", compact('user'));
+    }
+
+    public function update(User $user)
+    {
+        $data = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'url' => 'url',
+            'image ' => '',
+        ]);
+
+        $user->profile->update($data);
+
+        return redirect("/profile/{$user->id}");
+    }
+
 }
